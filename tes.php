8e84fa7ad7aa475b0359ba3de274a213fced274e
Kopen <89531508+kopen1@users.noreply.github.com>
2022-03-04 03:31:49 +0700

    update tes.php

--- tes.php
+++ tes.php
@@ -4,6 +4,6 @@
 $link = "https://youtube.com/playlist?list=PLKmkwxhfdH9FHLW7NsVY-Mv0fb23IMJDg";
 $url = "https://api.youtubemultidownloader.com/playlist?url=$link&nextPageToken=";
 
-echo file_get_contents($url);
-
+$x = file_get_contents($url);
+print_r($x);
 ?>
