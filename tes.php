2d22224bec92918b0729c6129793a2bf3270d750
Kopen <89531508+kopen1@users.noreply.github.com>
2022-03-04 03:36:45 +0700

    update tes.php

--- tes.php
+++ tes.php
@@ -1,9 +1,30 @@
 <?php
 
+if(isset($_POST["go"])){
+$link = $_POST["url"];
 
-$link = "https://youtube.com/playlist?list=PLKmkwxhfdH9FHLW7NsVY-Mv0fb23IMJDg";
 $url = "https://api.youtubemultidownloader.com/playlist?url=$link&nextPageToken=";
 
-$x = file_get_contents($url);
+$x = json_decode(file_get_contents($url),1);
 print_r($x);
+}
 ?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <meta name="YouTube Download" content="Irkop">
+    <title></title>
+  </head>
+  <body>
+    
+    <form action="" method="post" accept-charset="utf-8">
+      <input type="text" name="url" value="" />
+      <input type="button" name="go" id="go" value=" Download " />
+      
+    </form>
+    
+    
+  </body>
+</html>
