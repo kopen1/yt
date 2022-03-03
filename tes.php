084afda132c26021a94ed53e0f930584061563f3
Kopen <89531508+kopen1@users.noreply.github.com>
2022-03-04 03:44:43 +0700

    update tes.php

--- tes.php
+++ tes.php
@@ -7,6 +7,8 @@ $url = "https://api.youtubemultidownloader.com/playlist?url=$link&nextPageToken=
 
 $x = json_decode(file_get_contents($url),1);
 print_r($x);
+}else{
+  echo " input kosong ";
 }
 ?>
 
