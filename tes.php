0d1926f3f3dc46bc272a41274bf5cc476d44a6ec
Kopen <89531508+kopen1@users.noreply.github.com>
2022-03-04 03:58:56 +0700

    update tes.php

--- tes.php
+++ tes.php
@@ -1,4 +1,14 @@
+<?php
 
+if(isset($_POST["go"])){
+$link = $_POST["url"];
+$url = "https://api.youtubemultidownloader.com/playlist?url=$link&nextPageToken=";
+$x = json_decode(file_get_contents($url),1);
+print_r($x);
+}else{
+  $msg = true;$text = " URL INVALID ";
+}
+?>
 <!DOCTYPE html>
 <html>
   <head>
@@ -8,6 +18,7 @@
   </head>
   <body>
     
+    <?php if($msg==true) echo $txt; ?>
     <form action="dl.php" method="post" >
       <input type="url" name="url" value="" />
       <input type="submit" name="go" value=" Download " />
