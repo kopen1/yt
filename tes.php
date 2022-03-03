e06b2ba3e08719d481db8b97898c2cf9eb424f18
Kopen <89531508+kopen1@users.noreply.github.com>
2022-03-04 03:46:27 +0700

    update tes.php

--- tes.php
+++ tes.php
@@ -1,16 +1,3 @@
-<?php
-
-if(isset($_POST["go"])){
-echo $link = $_POST["url"];
-
-$url = "https://api.youtubemultidownloader.com/playlist?url=$link&nextPageToken=";
-
-$x = json_decode(file_get_contents($url),1);
-print_r($x);
-}else{
-  echo " input kosong ";
-}
-?>
 
 <!DOCTYPE html>
 <html>
@@ -21,7 +8,7 @@ print_r($x);
   </head>
   <body>
     
-    <form action="tes.php" method="post" >
+    <form action="dl.php" method="post" >
       <input type="url" name="url" value="" />
       <input type="button" name="go" id="go" value=" Download " />
       
